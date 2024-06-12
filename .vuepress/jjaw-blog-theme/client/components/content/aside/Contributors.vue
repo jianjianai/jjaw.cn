4d03283c4189337a0fac3dcf0f0dbec800909684
jjaw <jjaw@jjaw.cn>
2024-06-12 15:33:59 +0800

    新窗口打开贡献者

--- .vuepress/jjaw-blog-theme/client/components/content/aside/Contributors.vue
+++ .vuepress/jjaw-blog-theme/client/components/content/aside/Contributors.vue
@@ -18,7 +18,7 @@ function githubAvatarUrlAddSize(url:URL):string{
         </template>
         <template #default>
             <div class="contributors">
-                <a v-for="userinfo of articleGitHubInfo.gitHubContributors" class="contributor" :href="userinfo.githubUrl" :key="userinfo.email">
+                <a v-for="userinfo of articleGitHubInfo.gitHubContributors" class="contributor" :href="userinfo.githubUrl" :key="userinfo.email" target="_blank">
                     <div class="contributor-img">
                         <img class="contributor-img-img" v-if="userinfo.avatarUrl" :src="githubAvatarUrlAddSize(userinfo.avatarUrl)" :alt="userinfo.name">
                         <div  class="contributor-img-div" v-else>{{ userinfo.name.charAt(0) }}</div>
