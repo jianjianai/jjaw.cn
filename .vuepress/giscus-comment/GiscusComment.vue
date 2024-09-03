99d43d94f5afdcdf934425470f2b9442d5bf7b2f
jjaw <jjaw@jjaw.cn>
2024-09-03 16:40:06 +0800

    Giscus path更新自动刷新

--- .vuepress/giscus-comment/GiscusComment.vue
+++ .vuepress/giscus-comment/GiscusComment.vue
@@ -1,8 +1,11 @@
 <script setup>
 import Giscus from '@giscus/vue';
 import { config } from '@temp/giscus-comment/config'
+import { useRoute } from 'vue-router';
+
+const router = useRoute();
 
 </script>
 <template>
-    <Giscus v-bind="config"/>
+    <Giscus v-bind="config" :key="router.path"/>
 </template>
